1d5b543822464e8c8524caafe081ace35d2f82f4
AyushVarshneylovesCoding <av5277670@gmail.comm>
2022-10-07 02:11:38 +0530

    third commit

--- Task3/new.py
+++ Task3/new.py
@@ -1,6 +1,6 @@
 
 
-print("hey")
+
 s=input()
 n=int(input())
 list1=[]
@@ -11,14 +11,17 @@ for x in range(n):
     list1.append("")
 k=2*n-2
 for x in range(len(s)):
-    k1=x%k
+    k1=x%k             #after every k=2*n-2 positions the row index to which character to be appended will repeat
     if(k1<=n-2):
-        list1[k1]+=s[x]
+        list1[k1]+=s[x] #k1 acts as a referece by which we calculate the row index , in it's less than n-1 it will itself act as row index, otherwise "how much distant is from k " will act as row index
+
     else:
         k1=k-k1
-        list1[k1]+=s[x]
+        list1[k1]+=s[x] 
 res=""
-print(list1)
+
 for x in range(len(list1)):
     res+=list1[x]
-print(res)
+
+print("input string:",s)
+print("output string:",res)
